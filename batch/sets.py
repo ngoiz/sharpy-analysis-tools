02aa94181cc3cd24e772ad378a8c8501ce142692
Norberto Goizueta <ng213@ic.ac.uk>
2020-07-23 13:14:09 +0100

    Support for legacy eig file and wing tip deflection of entire set

--- batch/sets.py
+++ batch/sets.py
@@ -18,9 +18,13 @@ class Actual:
         self.aerodynamic = self.cases['aerodynamic']
         self.aeroelastic = self.cases['aeroelastic']
 
-    def load_bulk_cases(self, *args, replace_dir=None, append=False):
+    def load_bulk_cases(self, *args, replace_dir=None, append=False, **kwargs):
 
         source_cases_name = glob.glob(self.path)
+
+        eigs_legacy = kwargs.get('eigs_legacy', True)
+
+        n_loaded_cases = 0
         for source in source_cases_name:
             try:
                 param_file = glob.glob(source + '/*.pmor.sharpy')[0]
@@ -46,7 +50,12 @@ class Actual:
 
                 case = Case(param_value, sys, parameter_name=self.param_name, path_to_data=path_to_source_case)
                 case.name = case_info['sim_info']['case']
-                case.path_to_sys['eigs'] = case.path + '/stability/{:s}_eigenvalues.dat'.format(sys)
+
+                if eigs_legacy: # asymtotic stability in dev_pmor has an extra setting to save aeroelastic_eigenvalues.dat
+                    case.path_to_sys['eigs'] = case.path + '/stability/eigenvalues.dat'
+                else:
+                    case.path_to_sys['eigs'] = case.path + '/stability/{:s}_eigenvalues.dat'.format(sys)
+
                 case.path_to_sys['freqresp'] = case.path + '/frequencyresponse/{:s}.freqresp.h5'.format(sys)
                 case.path_to_sys['ss'] = case.path + '/statespace/{:s}.statespace.dat'.format(sys)
                 try:
@@ -69,6 +78,8 @@ class Actual:
                 if 'beam_modal_analysis' in args:
                     case.load_beam_modal_analysis()
                 self.cases[sys].add_case(param_value, case)
+                n_loaded_cases += 1
+        print('Loaded {} cases'.format(n_loaded_cases))
 
     def eigs(self, sys):
         param_array = []
@@ -78,6 +89,33 @@ class Actual:
             param_array.append(np.ones_like(case.eigs[:, 0]) * case.parameter_value)
 
         return np.concatenate(param_array), np.concatenate(eigs)
+
+    def wing_tip_deflection(self, frame='a', alpha=0):
+        param_array = []
+        deflection = []
+
+        if frame == 'g':
+            try:
+                import sharpy.utils.algebra as algebra
+            except ModuleNotFoundError:
+                raise(ModuleNotFoundError('Please load sharpy'))
+            else:
+                cga = algebra.quat2rotation(algebra.euler2quat(np.array([0, alpha * np.pi / 180, 0])))
+
+        for case in self.cases['aeroelastic']:
+            param_array.append(case.parameter_value)
+            if frame == 'a':
+                deflection.append(case.deflection[-1, -3:])
+            elif frame == 'g':
+                deflection.append(cga.dot(case.deflection[-1, -3:]))
+
+        param_array = np.array(param_array)
+        order = np.argsort(param_array)
+        param_array = param_array[order]
+        deflection = np.array([deflection[ith] for ith in order])
+
+        return param_array, deflection
+
 
 
 class SetIterator:
@@ -125,6 +163,9 @@ class SetOfCases:
     def __iter__(self):
         return SetIterator(self)
 
-    def find_parameter_value(self, param_value):
+    def find_parameter_value(self, param_value, return_idx=False):
         ind = self.parameter_values.index(param_value)
-        return self(ind)
+        if not return_idx:
+            return self(ind)
+        else:
+            return ind
